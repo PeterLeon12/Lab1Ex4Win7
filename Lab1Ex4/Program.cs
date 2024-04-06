8d15327a98338759ec33e7e0e9e22f2d863997f6
user <user@DESKTOP-FN026Q4>
2024-04-06 15:24:59 +0300

    Add project files.

---
+++ Lab1Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Ex 4
+//Scrieti un program care va afisa semnul unui numar citit de la tastatura
+//• Daca numarul este pozitiv, va afisa “+”
+//• Daca numarul este negativ, va afisa “-”
+//• Daca numarul este 0, va afisa “0
+//
+
+
+Console.WriteLine("Scrieti un numar:");
+int numar = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Numarul" + " " + numar + " " + "este ");
+
+
+
+if (numar > 0)
+{
+    Console.WriteLine("+");
+}
+else if (numar < 0)
+{
+    Console.WriteLine("-");
+}
+else
+{
+    Console.WriteLine("0");
+}
+
